2f4e33fff8d475bdc94a0492be2fecea697c9c13
yljvon <1689606211@qq.com>
2018-12-19 09:16:51 +0800

    短信服务

---
+++ MessageTest/src/main/java/com/message/Message.java
@@ -0,0 +1,42 @@
+package com.message;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+
+public class Message {
+	public static void main(String[] args) {
+	    String host = "http://yzx.market.alicloudapi.com";
+	    String path = "/yzx/sendSms";
+	    String method = "POST";
+	    String appcode = "你自己的code";
+	    Map<String, String> headers = new HashMap<String, String>();
+	    //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+	    headers.put("Authorization", "APPCODE " + appcode);
+	    Map<String, String> querys = new HashMap<String, String>();
+	    querys.put("mobile", "你自己的手机号");
+	    querys.put("param", "code:1234");
+	    querys.put("tpl_id", "TP1710262");
+	    Map<String, String> bodys = new HashMap<String, String>();
+
+
+	    try {
+	    	/**
+	    	* 重要提示如下:
+	    	* HttpUtils请从
+	    	* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+	    	* 下载
+	    	*
+	    	* 相应的依赖请参照
+	    	* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+	    	*/
+	    	HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+	    	System.out.println(response.toString());
+	    	//获取response的body
+	    	//System.out.println(EntityUtils.toString(response.getEntity()));
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+	}
+}
